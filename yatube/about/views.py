ceb83777dd65dfe472baecf68395e12684b4dd56
tr202 <tr202@ya.ru>
2022-12-29 20:05:55 +0300

    Update review app

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -10,8 +10,6 @@ class AboutTechView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Здесь можно произвести какие-то действия для создания контекста.
-        # Для примера в словарь просто передаются две строки
         context['just_title'] = 'Очень простая страница'
         context['just_text'] = ('На создание этой страницы '
                                 'у меня ушло пять минут! Ай да я.')
